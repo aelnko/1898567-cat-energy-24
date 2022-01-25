bbac4084512f3e09862d883d5bc424bc4510e4a2
Елена <lekolupaeva1201@gmail.com>
2022-01-25 15:58:05 +0300

    делаю состояния кнопок

--- source/js/nav-button.js
+++ source/js/nav-button.js
@@ -1,6 +1,15 @@
 let navButton = document.querySelector('.main-nav__toggle');
 let mainNav = document.querySelector('.main-nav');
+let buttonText = document.querySelector('.main-nav__close-toggle--visually-hidden');
 
+mainNav.classList.remove('main-nav--no-js');
 navButton.onclick = function () {
   mainNav.classList.toggle('main-nav--shown');
+  navButton.classList.toggle('main-nav__toggle--close');
+  if (navButton.classList.contains('main-nav__toggle--close')) {
+    buttonText.textContent = "Закрыть меню";
+  } else {
+    buttonText.textContent = "Открыть меню";
+  }
 };
+
